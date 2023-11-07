fe18d331df916666f0a9d96ed70f5beba4ce109e
1526414 <1526414@uab.cat>
2023-11-07 03:20:10 +0100

    loads from PNGs

--- Plugin.cs
+++ Plugin.cs
@@ -18,40 +18,49 @@ namespace BorkelRNVG
     [BepInPlugin("com.borkel.nvgmasks", "my very humble attempt at replacing the damn nvg masks", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        public static Texture2D[] masks; //my modded masks will be loaded here
-        public AssetBundle bundle; //grabs the bundle with my masks
+        //public static Texture2D[] masks; //my modded masks will be loaded here
+        //public AssetBundle bundle; //grabs the bundle with my masks
+        public static Texture2D maskAnvis; 
+        public static Texture2D maskBino; 
+        public static Texture2D maskMono; 
         private void Awake()
         {
             //directory contains string of path where the .dll is located, for me it is C:\SPTarkov3.7.1\BepInEx\plugins
             string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            //realmasks is the bundle, Fontaine suggests to load the masks from PNGs isntead of a bundle, should change it
+            //loading from PNGs, like Fontaine suggested
             string bundlePath = $"{directory}\\BorkelRNVG\\realmasks";
-            Logger.LogMessage($"directory {directory} bundlepath {bundlePath}");
-            //Load all assets from the bundle realmasks located in C:\SPTarkov3.7.1\BepInEx\plugins\BorkelRNVG
-            if (File.Exists(bundlePath))
+            string anvisPath = $"{directory}\\BorkelRNVG\\PNGtextures\\mask_anvis.png";
+            string binoPath = $"{directory}\\BorkelRNVG\\PNGtextures\\mask_binocular.png";
+            string monoPath = $"{directory}\\BorkelRNVG\\PNGtextures\\mask_old_monocular.png";
+            maskAnvis = LoadPNG(anvisPath);
+            maskBino = LoadPNG(binoPath);
+            maskMono = LoadPNG(monoPath);
+            maskAnvis.wrapMode = TextureWrapMode.Clamp;
+            maskBino.wrapMode = TextureWrapMode.Clamp; //otherwise the mask will repeat itself around screen borders
+            maskMono.wrapMode = TextureWrapMode.Clamp;
+            Logger.LogMessage($"Texture2D 0: {maskAnvis}"); //mask 0: mask_anvis
+            Logger.LogMessage($"Texture2D 1: {maskBino}"); //mask 1: mask_binocular
+            Logger.LogMessage($"Texture2D 2: {maskMono}"); //mask 2: mask_old_monocular
+            if(maskAnvis == null || maskBino == null || maskMono==null)
             {
-                bundle = AssetBundle.LoadFromFile(bundlePath); //loads the modified bundle resources.assets
-                if (bundle != null)
-                {
-                    Logger.LogMessage($"---BUNDLE LOADED---");
-                    masks = bundle.LoadAllAssets<Texture2D>(); //loads all assets, including the important ones: mask_anvis, mask_binocular, mask_old_monocular
-                    masks[0].wrapMode = TextureWrapMode.Clamp; 
-                    masks[1].wrapMode = TextureWrapMode.Clamp; //otherwise the mask will repeat itself around screen borders
-                    masks[2].wrapMode = TextureWrapMode.Clamp;
-                    Logger.LogMessage($"Texture2D 0: {masks[0].name}"); //mask 0: mask_anvis
-                    Logger.LogMessage($"Texture2D 1: {masks[1].name}"); //mask 1: mask_binocular
-                    Logger.LogMessage($"Texture2D 2: {masks[2].name}"); //mask 2: mask_old_monocular
-                    new SetMaskPatch().Enable();
-                }
-                else
-                {
-                    Logger.LogError("Failed to load the asset bundle.");
-                }
+                Logger.LogError($"Error loading PNGs.");
+                return;
             }
-            else
+            new SetMaskPatch().Enable();
+        }
+        public static Texture2D LoadPNG(string filePath)
+        {
+
+            Texture2D tex = null;
+            byte[] fileData;
+
+            if (File.Exists(filePath))
             {
-                Logger.LogError("Asset bundle not found.");
+                fileData = File.ReadAllBytes(filePath);
+                tex = new Texture2D(2, 2);
+                tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
             }
+            return tex;
         }
     }
     public class SetMaskPatch : ModulePatch {  //this will patch the instance of the NightVision class, thanks Fontaine, Mirni, Cj, GrooveypenguinX, Choccster, kiobu-kouhai, GrakiaXYZ, kiki, Props (sorry if i forget someone)
@@ -69,9 +78,9 @@ namespace BorkelRNVG
             Logger.LogMessage($"Binosmask name: {__instance.BinocularMaskTexture.name}");
             Logger.LogMessage($"Monomask name: {__instance.OldMonocularMaskTexture.name}");
             //replaces the masks in the class NightVision
-            __instance.AnvisMaskTexture = Plugin.masks[0];
-            __instance.BinocularMaskTexture = Plugin.masks[1];
-            __instance.OldMonocularMaskTexture = Plugin.masks[2];
+            __instance.AnvisMaskTexture = Plugin.maskAnvis;
+            __instance.BinocularMaskTexture = Plugin.maskBino;
+            __instance.OldMonocularMaskTexture = Plugin.maskMono;
             Logger.LogMessage($"After Mask name: {__instance.Mask.name}");
             Logger.LogMessage($"After Anvismask name: {__instance.AnvisMaskTexture.name}");
             Logger.LogMessage($"After Binosmask name: {__instance.BinocularMaskTexture.name}");
