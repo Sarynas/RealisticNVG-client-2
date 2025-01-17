16392c925dd60b02ae527b4f89d8144e5cee31ad
peinwastaken <legokid173@gmail.com>
2025-01-17 18:24:28 +0200

    its like a car with its engine taken out. that means it doesnt work. and its wip.

--- Helpers/AutoGatingController.cs
+++ Helpers/AutoGatingController.cs
@@ -1,7 +1,13 @@
-﻿using BSG.CameraEffects;
-using System.Management.Instrumentation;
+﻿using BorkelRNVG.Patches;
+using BSG.CameraEffects;
+using Comfort.Common;
+using EFT.UI;
+using EFT.Vaulting;
+using HarmonyLib;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 namespace BorkelRNVG
 {
@@ -10,45 +16,48 @@ namespace BorkelRNVG
         public static AutoGatingController Instance;
 
         // randomass variables
-        private Camera _mainCamera;
-        private Camera _rtCamera;
-        private NightVision _nightVision;
+        public Camera mainCamera;
+        //private Camera _rtCamera;
+        public NightVision nightVision;
 
         // shader stuff
         public ComputeShader computeShader;
         public ComputeBuffer brightnessBuffer;
         public RenderTexture renderTexture;
 
+        public CommandBuffer commandBuffer;
+        public int nvInt_6;
+        public RenderTargetIdentifier nvTargetIdentifier;
+        public Material nvMaterial_0;
+
+        private int _currentFrame = 0;
+        private float _currentBrightness = 1.0f;
+        private CameraEvent _cameraEvent = CameraEvent.BeforeImageEffects;
+
+        private const float BRIGHTNESS_SCALE = 10000f;
         private int kernel;
         private int textureWidth;
         private int textureHeight;
-
-        private const float BRIGHTNESS_SCALE = 10000f;
-
-        // more randomass vars
-        private int _currentFrame = 0;
-        private float _currentBrightness = 1.0f;
-
         private int _renderInterval = 60;
         private float _gateSpeed = 0.02f;
-        private int _brightnessExponent = 1;
+        private float _lerp1 = 1f;
+        private float _lerp2 = 0.2f;
+        private float minInput = 0.08f;
+        private float maxInput = 0.1f;
 
-        private float _lerp1 = 0.2f;
-        private float _lerp2 = 1f;
+        // more randomass vars
+        private int _brightnessExponent = 1;
 
         // screen was upside down.. very funny but not playable
         private Vector2 _uvScale = new Vector2(1, -1);
         private Vector2 _uvOffset = new Vector2(0, 1);
-
-        private float minInput = 0.08f;
-        private float maxInput = 0.1f;
 
         // public stuff
         public float GatingMultiplier { get; private set; } = 1.0f;
 
         public static AutoGatingController Create()
         {
-            GameObject camera = Camera.main.gameObject;
+            GameObject camera = CameraClass.Instance.Camera.gameObject;
             AutoGatingController autoGatingController = camera.AddComponent<AutoGatingController>();
             return autoGatingController;
         }
@@ -64,73 +73,123 @@ namespace BorkelRNVG
             Instance = this;
             DontDestroyOnLoad(gameObject);
 
-            _mainCamera = CameraClass.Instance.Camera;
-            _nightVision = CameraClass.Instance.NightVision;
+            textureWidth = Screen.width;
+            textureHeight = Screen.height;
+
+            mainCamera = CameraClass.Instance.Camera;
+            nightVision = CameraClass.Instance.NightVision;
 
             // everything beyond this point makes my head hurt
 
-            computeShader = AssetBundle.LoadFromFile($"{Plugin.assetsDirectory}\\Shaders\\pein_shaders").LoadAsset("BrightnessShader") as ComputeShader;
+            renderTexture = CreateRenderTexture();
 
-            textureWidth = 32;
-            textureHeight = 32;
+            commandBuffer = new CommandBuffer();
+            commandBuffer.SetRenderTarget(renderTexture);
+            commandBuffer.Blit(null, renderTexture);
+            mainCamera.AddCommandBuffer(CameraEvent.BeforeImageEffects, commandBuffer);
 
-            renderTexture = new RenderTexture(textureWidth, textureHeight, 0, RenderTextureFormat.DefaultHDR);
-            renderTexture.enableRandomWrite = true;
-            renderTexture.Create();
-
+            /*
             GameObject rtCameraGameObject = new GameObject("RTCamera");
             _rtCamera = rtCameraGameObject.AddComponent<Camera>();
             _rtCamera.CopyFrom(_mainCamera);
             _rtCamera.targetTexture = renderTexture;
             _rtCamera.nearClipPlane = 2f;
-            DontDestroyOnLoad(rtCameraGameObject);
+            _rtCamera.enabled = false;
+            DontDestroyOnLoad(rtCameraGameObject);*/
 
             brightnessBuffer = new ComputeBuffer(1, sizeof(uint));
 
+            //computeShader = AssetBundle.LoadFromFile($"{Plugin.assetsDirectory}\\Shaders\\pein_shaders").LoadAsset("BrightnessShader") as ComputeShader;
+            computeShader = AssetBundle.LoadFromFile($"{Plugin.assetsDirectory}\\Shaders\\pein_shaders").LoadAsset("AverageBrightnessShader") as ComputeShader;
             kernel = computeShader.FindKernel("CSReduceBrightness");
+            computeShader.SetInt("_Width", textureWidth);
+            computeShader.SetInt("_Height", textureHeight);
+            computeShader.SetTexture(kernel, "_InputTexture", renderTexture);
+            computeShader.SetBuffer(kernel, "_BrightnessBuffer", brightnessBuffer);
+
+            // temp rt debug
+            var canvas = new GameObject("Canvas", typeof(Canvas)).GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            var rawImage = new GameObject("RawImage", typeof(RawImage)).GetComponent<RawImage>();
+            rawImage.transform.SetParent(canvas.transform);
+
+            RectTransform rectTransform = rawImage.GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(1, 1);
+            rectTransform.anchorMax = new Vector2(1, 1);
+
+            rectTransform.anchoredPosition = new Vector2(-100, -100);
+
+            rawImage.texture = renderTexture;
         }
 
-        void Update()
+        private RenderTexture CreateRenderTexture()
         {
+            RenderTexture rt = new RenderTexture(textureWidth, textureHeight, 24, RenderTextureFormat.ARGB32);
+            rt.enableRandomWrite = true;
+            rt.useMipMap = false;
+            rt.autoGenerateMips = false;
+            rt.filterMode = FilterMode.Point;
+            rt.Create();
+
+            return rt;
+        }
+
+        private void Update()
+        {
+            /*
             // Ensure the renderTexture is set up properly
             if (renderTexture == null || !renderTexture.IsCreated())
             {
-                renderTexture = new RenderTexture(textureWidth, textureHeight, 0, RenderTextureFormat.DefaultHDR);
-                renderTexture.enableRandomWrite = true;
-                renderTexture.Create();
+                renderTexture = CreateRT();
             }
 
-            _rtCamera.transform.position = _mainCamera.transform.position;
-            _rtCamera.transform.rotation = _mainCamera.transform.rotation;
+            //_rtCamera.transform.position = _mainCamera.transform.position;
+            //_rtCamera.transform.rotation = _mainCamera.transform.rotation;
+            //_rtCamera.Render();
 
-            // Reset the buffer value
             uint[] brightnessSum = new uint[1] { 0 };
             brightnessBuffer.SetData(brightnessSum);
 
-            // Set shader parameters
-            computeShader.SetTexture(kernel, "_InputTexture", renderTexture);
-            computeShader.SetBuffer(kernel, "_BrightnessBuffer", brightnessBuffer);
-            computeShader.SetInt("_Width", textureWidth);
-            computeShader.SetInt("_Height", textureHeight);
-
-            // Dispatch the compute shader
-            int threadGroupsX = Mathf.CeilToInt(textureWidth / 8.0f);
-            int threadGroupsY = Mathf.CeilToInt(textureHeight / 8.0f);
-            computeShader.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
+            computeShader.Dispatch(kernel, 8, 8, 1);
 
             brightnessBuffer.GetData(brightnessSum);
 
             float totalPixels = textureWidth * textureHeight;
 
-            _currentBrightness = (brightnessSum[0] / (BRIGHTNESS_SCALE * totalPixels));
+            _currentBrightness = brightnessSum[0] / totalPixels;
 
             float normalizedBrightness = Mathf.Clamp((_currentBrightness - minInput) / (maxInput - minInput), 0.0f, 1.0f);
             float gatingTarget = Mathf.Lerp(_lerp1, _lerp2, normalizedBrightness);
 
             GatingMultiplier = Mathf.Lerp(GatingMultiplier, gatingTarget, _gateSpeed);
 
-            _nightVision.ApplySettings();
+            _nightVision.ApplySettings();*/
         }
+
+        private void OnPreCull()
+        {
+            //Graphics.ExecuteCommandBuffer(commandBuffer);
+            mainCamera.GetComponent<UltimateBloom>()?.method_23(null, renderTexture, 1f);
+        }
+
+        /*
+        private void OnPreCull()
+        {
+            commandBuffer.Blit(BuiltinRenderTextureType.CurrentActive, renderTexture);
+
+            Singleton<LevelSettings>.Instance.AmbientType = AmbientType.NightVision;
+            nvCommandBuffer.Clear();
+            nvCommandBuffer.GetTemporaryRT(nvInt_6, -1, -1);
+            nvCommandBuffer.Blit(nvTargetIdentifier, nvInt_6, _nightVision.Material_0);
+            nvCommandBuffer.Blit(nvInt_6, nvTargetIdentifier);
+            nvCommandBuffer.ReleaseTemporaryRT(nvInt_6);
+        }
+
+        private void OnPostRender()
+        {
+            Singleton<LevelSettings>.Instance.AmbientType = AmbientType.Default;
+        }*/
 
         private void OnDestroy()
         {
